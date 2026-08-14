edcc7c7322d1df70b187d426a768da1cff638747
trevorGit <your.email@westernsydney.edu.au>
2026-08-14 01:22:26 +0000

    test case 1

--- array-toolbox.mjs
+++ array-toolbox.mjs
@@ -6,3 +6,13 @@ export function sum(numbers){
     return total;
 }
 
+export function average(numbers){
+    if (numbers.length === 0){
+        return 0;
+    }
+    return sum(numbers) / numbers.length;
+}
+
+//test case 1: works
+console.log(average([2, 4, , 6]));
+console.log(average([]));
