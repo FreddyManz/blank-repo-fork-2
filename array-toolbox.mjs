96e8b7d54bcba69aef3e7bc3c0207f6905845296
trevorGit <your.email@westernsydney.edu.au>
2026-08-14 01:56:13 +0000

    finsihed up last case

--- array-toolbox.mjs
+++ array-toolbox.mjs
@@ -1,3 +1,5 @@
+
+//for sum
 export function sum(numbers){
     let total = 0;
     for (const num of numbers){
@@ -5,9 +7,38 @@ export function sum(numbers){
     }
     return total;
 }
+
+//for average 
 export function average(numbers){
     if (numbers.length === 0){
         return 0;
     }
     return sum(numbers) / numbers.length;
+}
+
+//unique array
+export function unique(array){
+    return [...new Set(array)];
+}
+
+
+//chunky boi
+//Inspiration For My Answer: https://stackoverflow.com/questions/27857011/how-to-split-an-array-into-smaller-chunk-arrays
+
+export function chunk(array, size){
+    const result = [];
+    //up to 5
+    for (let i = 0; i < array.length; i += size){
+        result.push(array.slice(i,i + size));
+    }
+    return result;
+}
+
+//intersection helper
+//Inspiration: https://stackoverflow.com/questions/1885557/simplest-code-for-array-intersection-in-javascript
+export function intersection (firstArray, secondArray){
+    const secondSet = new Set(secondArray);
+    //is this value in the set?
+    const result = firstArray.filter(value => secondSet.has(value));
+    return unique(result);
 }
--- test.mjs
+++ test.mjs
@@ -1,5 +1,12 @@
-import { sum, average } from './array-toolbox.mjs';
+import { sum, average, unique, chunk, intersection} from './array-toolbox.mjs';
+
 
 console.log(sum([1, 2, 3, 4]));   // 10
 console.log(average([2, 4, 6]));  // 4
 console.log(average([]));          // 0
+
+
+console.log(unique([1, 2, 2, 3, 1])); // expect [1, 2, 3]
+
+console.log(chunk([1, 2, 3, 4, 5], 2));
+console.log(intersection([1, 2, 2, 3], [2, 3, 4]));
